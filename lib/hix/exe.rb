2b7de0a90bd6093984bf33cd78241656491317ea
Wiktor Plaga <wiktor.plaga@tzif.io>
2022-10-11 04:22:31 +0200

    251745 - ADD gems "hix version" command

--- lib/hix/exe.rb
+++ lib/hix/exe.rb
@@ -6,6 +6,7 @@ module Hix
   module Exe
     LOGIN = "login"
     START = "start"
+    VERSION = "version"
   end
 end
 
@@ -16,6 +17,8 @@ when Hix::Exe::LOGIN
   log.say_status(:ok, "Loging in", :green)
 when Hix::Exe::START
   log.say_status(:ok, "Starting new project", :green)
+when Hix::Exe::VERSION
+  puts Hix::Version::STRING
 else
   log.say_status(:error, "command does not exist", :red)
 end
